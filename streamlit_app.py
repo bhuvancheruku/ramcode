9e892046cce11b433a5cf8039b2b5396e27fb657
bhuvan cheruku <34746125+bhuvancheruku@users.noreply.github.com>
2023-12-06 20:16:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import mysql.connector
 from datetime import datetime
 
 st.set_page_config(page_title='ATM Application', page_icon=':bank:')
@@ -23,21 +22,21 @@ def login():
 def signup():
     st.write("Signup Page")
 
-def view_balance():
+# Function to fetch data from the database
+@st.cache(allow_output_mutation=True)
+def fetch_data(user):
     font = "<font size='3' color='black'>"
     output = ""
-    con = mysql.connector.connect(host='127.0.0.1', port=3306, user='root', password='', database='atm')
-    with con:
-        cur = con.cursor()
-        cur.execute(f"select * FROM transaction where username='{uname}'")
-        rows = cur.fetchall()
-        for row in rows:
-            output += f"<tr><td>{font}{str(row[0])}</font></td>"
-            output += f"<td>{font}{str(row[1])}</font></td>"
-            output += f"<td>{font}{str(row[2])}</font></td>"
-            output += f"<td>{font}{str(row[3])}</font></td>"
-            output += f"<td>{font}{str(row[4])}</font></td>"
-    st.write(output)
+    # Simulate data fetching from the database
+    # Replace this with your actual data fetching logic
+    rows = [("username", "amount", "type", "date", "balance")]  # Example data
+    for row in rows:
+        output += f"<tr><td>{font}{str(row[0])}</font></td>"
+        output += f"<td>{font}{str(row[1])}</font></td>"
+        output += f"<td>{font}{str(row[2])}</font></td>"
+        output += f"<td>{font}{str(row[3])}</font></td>"
+        output += f"<td>{font}{str(row[4])}</font></td>"
+    return output
 
 def login_action():
     global uname
@@ -45,20 +44,18 @@ def login_action():
     password = st.text_input("Password", type="password")
     data = st.file_uploader("Upload Fingerprint Image", type=["png"])
     index = 0
-    con = mysql.connector.connect(host='127.0.0.1', port=3306, user='root', password='', database='atm')
-    with con:
-        cur = con.cursor()
-        cur.execute("select * FROM users")
-        rows = cur.fetchall()
-        for row in rows:
-            if row[0] == user and password == row[1]:
-                in_file = open(f"C:/Users/marut/OneDrive/Desktop/FingerprintATM/static/users/{user}.png", "rb")
-                avail_data = in_file.read()
-                in_file.close()
-                if avail_data == data:
-                    index = 1
-                    uname = user
-                    break
+    # Simulate user data fetching from the database
+    # Replace this with your actual user authentication logic
+    users = [("username", "password")]  # Example data
+    for row in users:
+        if row[0] == user and password == row[1]:
+            in_file = open(f"C:/Users/marut/OneDrive/Desktop/FingerprintATM/static/users/{user}.png", "rb")
+            avail_data = in_file.read()
+            in_file.close()
+            if avail_data == data:
+                index = 1
+                uname = user
+                break
     if index == 0:
         st.warning("Invalid login details")
     else:
@@ -66,15 +63,13 @@ def login_action():
 
 def get_amount(user):
     amount = 0
-    con = mysql.connector.connect(host='127.0.0.1', port=3306, user='root', password='', database='atm')
-    with con:
-        cur = con.cursor()
-        cur.execute("select * FROM transaction")
-        rows = cur.fetchall()
-        for row in rows:
-            if row[0] == user:
-                amount = float(row[4])
-                break
+    # Simulate data fetching from the database
+    # Replace this with your actual data fetching logic
+    rows = [("username", "amount")]  # Example data
+    for row in rows:
+        if row[0] == user:
+            amount = float(row[1])
+            break
     return amount
 
 def withdraw_action():
@@ -86,13 +81,9 @@ def withdraw_action():
     if total > withdraw:
         total = total - withdraw
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        student_sql_query = f"update transaction set transaction_amount='{amount}',transaction_type='Withdrawl',transaction_date='{timestamp}',total_balance='{total}' where username='{user}'"
-        db_connection = mysql.connector.connect(host='127.0.0.1', port=3306, user='root', password='', database='atm')
-        db_cursor = db_connection.cursor()
-        db_cursor.execute(student_sql_query)
-        db_connection.commit()
-        if db_cursor.rowcount == 1:
-            status = "Withdrawl Transaction Successful"
+        # Simulate update transaction in the database
+        # Replace this with your actual update logic
+        status = "Withdrawal Transaction Successful"
     else:
         status = "Insufficient Fund"
     st.success(status)
@@ -105,23 +96,15 @@ def deposit_action():
     if total == 0:
         total = total + float(amount)
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        student_sql_query = f"INSERT INTO transaction(username,transaction_amount,transaction_type,transaction_date,total_balance) VALUES('{user}','{amount}','Deposit','{timestamp}','{total}')"
-        db_connection = mysql.connector.connect(host='127.0.0.1', port=3306, user='root', password='', database='atm')
-        db_cursor = db_connection.cursor()
-        db_cursor.execute(student_sql_query)
-        db_connection.commit()
-        if db_cursor.rowcount == 1:
-            status = "Transaction Successful"
+        # Simulate insert transaction in the database
+        # Replace this with your actual insert logic
+        status = "Transaction Successful"
     elif total > 0:
         total = total + float(amount)
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        student_sql_query = f"update transaction set transaction_amount='{amount}',transaction_type='Deposit',transaction_date='{timestamp}',total_balance='{total}' where username='{user}'"
-        db_connection = mysql.connector.connect(host='127.0.0.1', port=3306, user='root', password='', database='atm')
-        db_cursor = db_connection.cursor()
-        db_cursor.execute(student_sql_query)
-        db_connection.commit()
-        if db_cursor.rowcount == 1:
-            status = "Transaction Successful"
+        # Simulate update transaction in the database
+        # Replace this with your actual update logic
+        status = "Transaction Successful"
     st.success(status)
 
 def signup_action():
@@ -133,27 +116,44 @@ def signup_action():
     gender = st.radio("Gender", ["Male", "Female", "Other"])
     data = st.file_uploader("Upload Fingerprint Image", type=["png"])
     status = "none"
-    con = mysql.connector.connect(host='127.0.0.1', port=3306, user='root', password='', database='atm')
-    with con:
-        cur = con.cursor()
-        cur.execute("select * FROM users")
-        rows = cur.fetchall()
-        for row in rows:
-            if row[0] == user:
-                status = user + " Username already exists"
-                break
+    # Simulate user data fetching from the database
+    # Replace this with your actual user check logic
+    users = [("username")]  # Example data
+    for row in users:
+        if row[0] == user:
+            status = user + " Username already exists"
+            break
     if status == 'none':
-        db_connection = mysql.connector.connect(host='127.0.0.1', port=3306, user='root', password='', database='atm')
-        db_cursor = db_connection.cursor()
-        student_sql_query = f"INSERT INTO users(username,password,contact_no,emailid,address,gender) VALUES('{user}','{password}','{phone}','{email}','{address}','{gender}')"
-        db_cursor.execute(student_sql_query)
-        db_connection.commit()
-        if db_cursor.rowcount == 1:
-            out_file = open(f"C:/Users/marut/OneDrive/Desktop/FingerprintATM/static/users/{user}.png", "wb")
-            out_file.write(data)
-            out_file.close()
-            status = 'Signup process completed'
+        # Simulate insert user in the database
+        # Replace this with your actual insert logic
+        status = 'Signup process completed'
     st.success(status)
 
 def logout():
     st.write("Logout Page")
+
+# Streamlit app
+def main():
+    page = st.sidebar.selectbox("Choose a page", ["Home", "Login", "Signup", "Deposit", "Withdraw", "View Balance", "Logout"])
+
+    if page == "Home":
+        index()
+    elif page == "Login":
+        login()
+        login_action()
+    elif page == "Signup":
+        signup()
+        signup_action()
+    elif page == "Deposit":
+        deposit()
+        deposit_action()
+    elif page == "Withdraw":
+        withdraw()
+        withdraw_action()
+    elif page == "View Balance":
+        view_balance()
+    elif page == "Logout":
+        logout()
+
+if __name__ == "__main__":
+    main()
